cc78315d230f1b799da0fc02b02e1ba641d907a8
Anatolii Marandiuc <shamaneeel@mail.ru>
2019-10-25 03:02:37 +0300

    test

--- src/com/inert/Main.java
+++ src/com/inert/Main.java
@@ -1,22 +1,27 @@
 package com.inert;
 
-import com.inert.help.FileHelper;
-import com.inert.programSearch.ProgramSearch;
 
+import com.inert.programSearch.Program;
+import com.inert.programSearch.ProgramSearch;
+import com.inert.programSearch.ProgramSearchMultiThread;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, IOException {
         // TODO Есть класс для открытия ассоцириованных программ, на всякий случай
 
         //FileHelper.scriptStart();
 
-        List<String> paths;
-        ProgramSearch programSearch = new ProgramSearch();
-        paths = programSearch.getPaths();
-        for (String name : paths) {
-            System.out.println(name);
+//        Scanner scanner = new Scanner(System.in);
+//        scanner.nextInt();
+        ProgramSearch programSearch = new ProgramSearchMultiThread();
+        for (Map.Entry<String, Program> x : programSearch.getPaths().entrySet()) {
+            System.out.println(x.getValue().toString());
         }
     }
 }
